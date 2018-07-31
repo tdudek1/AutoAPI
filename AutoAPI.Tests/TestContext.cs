46cfb27b6b1a2f79bb80597309275437470e1ea8
Thomas Dudek <thomas.dudek@gmail.com>
2018-07-31 14:58:36 -0500

    Testing data controller basic tests

--- AutoAPI.Tests/TestContext.cs
+++ AutoAPI.Tests/TestContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace AutoAPI.Tests
@@ -21,7 +22,8 @@ namespace AutoAPI.Tests
     public class Author
     {
         [Key]
-        public int Id { get; set; }
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
         public String Name { get; set; }
         public List<Book> Books { get; set; }
 
