71f07c1ba77df0786f5c4a1c78eb98f274cf70db
Thomas Dudek <thomas.dudek@gmail.com>
2018-08-05 12:24:02 -0500

    Add Unit Tests

--- AutoAPI.Tests/RequestProcessorTest.cs
+++ AutoAPI.Tests/RequestProcessorTest.cs
@@ -28,10 +28,115 @@ namespace AutoAPI.Tests
             //act
             var result = (new RequestProcessor()).GetFilter(entityList.Where(x => x.Route == "authors").First(), new QueryCollection(queryString));
 
+            //assert
             Assert.Equal("Name == @0", result.Expression);
             Assert.Single(result.Values);
             Assert.Equal("Ernest Hemingway", (string)result.Values[0]);
 
+        }
+
+        [Fact]
+        public void BuildFilter_WhenTwoProperties_ThenFilter()
+        {
+            //arrange
+            var queryString = new Dictionary<string, StringValues>();
+            queryString.Add("filter[Name]", "Ernest Hemingway");
+            queryString.Add("filter[DateOfBirth]", "7/21/1899");
+
+            //act
+            var result = (new RequestProcessor()).GetFilter(entityList.Where(x => x.Route == "authors").First(), new QueryCollection(queryString));
+
+            //assert
+            Assert.Equal("Name == @0 && DateOfBirth == @1", result.Expression);
+            Assert.Equal(2, result.Values.Count());
+            Assert.Equal("Ernest Hemingway", (string)result.Values[0]);
+            Assert.Equal(new DateTime(1899, 7, 21), (DateTime)result.Values[1]);
+
+        }
+
+
+        [Fact]
+        public void BuildFilter_NoFilters_ThenNull()
+        {
+            //arrange
+            var queryString = new Dictionary<string, StringValues>();
+
+            //act
+            var result = (new RequestProcessor()).GetFilter(entityList.Where(x => x.Route == "authors").First(), new QueryCollection(queryString));
+
+            Assert.Null(result.Expression);
+        }
+
+        [Fact]
+        public void BuildSort_WhenTwoProperties_ThenFilter()
+        {
+            //arrange
+            var queryString = new Dictionary<string, StringValues>();
+            queryString.Add("sort[Name]", "desc");
+            queryString.Add("sort[DateOfBirth]", "asc");
+
+            //act
+            var result = (new RequestProcessor()).GetSort(entityList.Where(x => x.Route == "authors").First(), new QueryCollection(queryString));
+
+            //assert
+            Assert.Equal("Name desc, DateOfBirth asc", result);
+
+        }
+
+        [Fact]
+        public void BuildSort_NoSorts_ThenNull()
+        {
+            //arrange
+            var queryString = new Dictionary<string, StringValues>();
+
+            //act
+            var result = (new RequestProcessor()).GetSort(entityList.Where(x => x.Route == "authors").First(), new QueryCollection(queryString));
+
+            //assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void BuildPaging_NoPaging_ThenZerot()
+        {
+            //arrange
+            var queryString = new Dictionary<string, StringValues>();
+
+            //act
+            var result = (new RequestProcessor()).GetPaging(new QueryCollection(queryString));
+
+            Assert.Equal(0, result.Take);
+        }
+
+        [Fact]
+        public void BuildPaging_PageSize10_ThenTakeTen()
+        {
+            //arrange
+            var queryString = new Dictionary<string, StringValues>();
+            queryString.Add("pageSize", "10");
+
+            //act
+            var result = (new RequestProcessor()).GetPaging(new QueryCollection(queryString));
+
+            //assert
+            Assert.Equal(10, result.Take);
+            Assert.Equal(0, result.Skip);
+        }
+
+        [Fact]
+        public void BuildPaging_PageSize10AndPage3_ThenTakeTenSkip3()
+        {
+            //arrange
+            var queryString = new Dictionary<string, StringValues>();
+            queryString.Add("pageSize", "10");
+            queryString.Add("page", "3");
+
+            //act
+            var result = (new RequestProcessor()).GetPaging(new QueryCollection(queryString));
+
+            //assert
+            Assert.Equal(10, result.Take);
+            Assert.Equal(20, result.Skip);
         }
     }
 }
